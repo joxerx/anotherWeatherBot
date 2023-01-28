a5ed7c03e779a940b108405ab07f0e388c4aaebc
joxerx <n.lebedinsky@bk.ru>
2023-01-28 20:48:51 +0300

    Fix added classes, modules and methods

---
+++ api_service.py
@@ -0,0 +1,227 @@
+import json
+from dataclasses import dataclass
+from datetime import datetime
+from enum import StrEnum
+
+import requests
+
+import config
+from coordinates import Coordinates
+
+
+condition_dict = {
+    'clear': 'ясно',
+    'partly-cloudy': 'малооблачно',
+    'cloudy': 'облачно с прояснениями',
+    'overcast': 'пасмурно',
+    'drizzle': 'морось',
+    'light-rain': 'небольшой дождь',
+    'rain': 'дождь',
+    'moderate-rain': 'умеренно сильный дождь',
+    'heavy-rain': 'сильный дождь',
+    'continuous-heavy-rain': 'длительный сильный дождь',
+    'showers': 'ливень',
+    'wet-snow': 'дождь со снегом',
+    'light-snow': 'небольшой снег',
+    'snow': 'снег',
+    'snow-showers': 'снегопад',
+    'hail': 'град',
+    'thunderstorm': 'гроза',
+    'thunderstorm-with-rain': 'дождь с грозой',
+    'thunderstorm-with-hail': 'гроза с градом'
+}
+wind_dir_dict = {
+    'nw': 'северо-западное',
+    'n': 'северное',
+    'ne': 'северо-восточное',
+    'e': 'восточное',
+    'se': 'юго-восточное',
+    's': 'южное',
+    'sw': 'юго-западное',
+    'w': 'западное',
+    'c': 'штиль'
+}
+daytime_dict = {
+    'd': 'день',
+    'n': 'ночь'
+}
+prec_type_dict = {
+    0: 'без осадков',
+    1: 'дождь',
+    2: 'дождь со снегом',
+    3: 'снег',
+    4: 'град'
+}
+prec_strength_dict = {
+    0: '',
+    0.25: 'слабый ',
+    0.5: '',
+    0.75: 'сильный ',
+    1: 'очень сильный '
+}
+cloudness = {
+    0: 'ясно',
+    0.25: 'малооблачно',
+    0.5: 'облачно с прояснениями',
+    0.75: 'облачно с прояснениями',
+    1: 'пасмурно'
+}
+
+
+@dataclass(slots=True, frozen=True)
+class FactWeather:
+    temp: float  # cesium
+    feels_like: float
+    condition: str  # need to convert to words
+    wind_speed: float  # m/s
+    wind_gust: float
+    wind_dir: str  # need to convert words
+    pressure: float  # mm
+    humidity: float  # need to convert to percent
+    daytime: str  # need to convert day/night
+    obs_time: datetime
+    prec_type: int  # need to convert to str
+    prec_strength: float  # need to identify and show strength
+    cloudness: str  # need to convert to words
+
+
+@dataclass(slots=True, frozen=True)
+class Part:
+    temp_min: float
+    temp_max: float
+    temp_avg: float
+    feels_like: float
+    condition: str
+    wind_speed: float
+    wind_gust: float
+    wind_dir: str
+    pressure: float
+    humidity: float
+    prec_mm: float  # expected mm precipitation
+    prec_type: int  # need to convert to type of precipitation
+    prec_strength: float  # need to convert to words type and strength
+    cloudness: float  # need to convert to words
+
+
+@dataclass(slots=True, frozen=True)
+class Short:
+    temp: float
+    temp_min: float
+    feels_like: float
+    condition: str  # need to convert to words
+    wind_speed: float
+    wind_gust: float
+    wind_dir: str
+    pressure: float
+    humidity: float
+    prec_mm: float  # expected mm precipitation
+    prec_type: int  # need to convert to type of precipitation
+    prec_strength: float  # need to convert to words type and strength
+    cloudness: float  # need to convert to words
+
+
+@dataclass(slots=True, frozen=True)
+class Parts:
+    night: Part
+    morning: Part
+    day: Part
+    evening: Part
+    day_short: Short
+    night_short: Short
+
+
+@dataclass(slots=True, frozen=True)
+class Forecast:
+    date_ts: datetime  # unix time
+    week: int
+    sunrise: str
+    sunset: str
+    moon_code: int  # need to convert to words
+    parts: Parts
+
+
+@dataclass(slots=True, frozen=True)
+class CityInfo:
+    def_pressure: float
+    url: str
+
+
+@dataclass(slots=True, frozen=True)
+class Answer:
+    now: datetime
+    info: CityInfo
+    geo_object: str
+    fact: FactWeather
+    forecasts: Forecast
+
+
+def get_city_info(ya_weather_dict: dict) -> CityInfo:
+    return CityInfo(
+        def_pressure=ya_weather_dict['info']['def_pressure_mm'],
+        url=ya_weather_dict['info']['url']
+    )
+
+
+def get_fact_weather(ya_weather_dict: dict) -> FactWeather:
+    return FactWeather(
+        temp=ya_weather_dict['fact']['temp'],
+        feels_like=ya_weather_dict['fact']['feels_like'],
+        condition=condition_dict[ya_weather_dict['fact']['condition']],
+        wind_speed=ya_weather_dict['fact']['wind_speed'],
+        wind_gust=ya_weather_dict['fact']['wind_gust'],
+        wind_dir=wind_dir_dict[ya_weather_dict['fact']['wind_dir']],
+        pressure=ya_weather_dict['fact']['pressure_mm'],
+        humidity=ya_weather_dict['fact']['humidity'],
+        daytime=daytime_dict[ya_weather_dict['fact']['daytime']],
+        obs_time=datetime.fromtimestamp(ya_weather_dict['fact']['obs_time']),
+        prec_type=ya_weather_dict['fact']['humidity'],
+        prec_strength=ya_weather_dict['fact']['humidity'],
+        cloudness=cloudness[ya_weather_dict['fact']['cloudness']]
+    )
+
+
+def get_one_part(ya_weather_dict: dict) ->
+
+
+def get_parts_forecast(ya_weather_dict: dict) -> Parts:
+    return Parts(
+        morning=get_one_part()
+    )
+
+
+def get_forecasts(ya_weather_dict: dict) -> Forecast:
+    return Forecast(
+        date_ts=datetime.fromtimestamp(ya_weather_dict['forecasts'][0]['date_ts']),
+        week=ya_weather_dict['forecasts'][0]['week'],
+        sunrise=ya_weather_dict['forecasts'][0]['sunrise'],
+        sunset=ya_weather_dict['forecasts'][0]['sunset'],
+        moon_code=ya_weather_dict['forecasts'][0]['moon_code'],
+        parts=get_parts_forecast(ya_weather_dict['forecasts'][0]['parts'])
+    )
+
+
+def parse_string_answer(ya_weather_response: str) -> Answer:
+    ya_weather_dict = json.loads(ya_weather_response)
+    return Answer(
+        now=datetime.fromtimestamp(ya_weather_dict['now']),
+        info=get_city_info(ya_weather_dict),
+        geo_object=ya_weather_dict['geo_object']['district']['name'],
+        fact=get_fact_weather(ya_weather_dict),
+        forecasts=get_forecasts(ya_weather_dict)
+    )
+
+
+def get_full_answer(coordinates=Coordinates) -> Answer:
+    """"Generates Answer as object to manipulate with"""
+    yandex_response = get_yandex_weather_response(
+        longitude=coordinates.longitude,
+        latitude=coordinates.latitude
+    )
+    full_answer = parse_string_answer(yandex_response)
+    return full_answer
+
+
+def get_yandex_weather_response(latitude: float, longitude: float) -> str:
+    url = config.HEADER_YANDEX_API_CALL.format(latitude=latitude, longitude=longitude)
+    return requests.get(url, headers=config.HEADER_YANDEX_API_CALL).text
+
---
+++ bot.py
@@ -0,0 +1,37 @@
+import logging
+from aiogram import Bot, Dispatcher, executor, types, utils
+
+import config
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# Initialize bot and dispatcher
+bot = Bot(token=config.BOT_API_TOKEN, parse_mode="html")
+dp = Dispatcher(bot)
+
+
+def get_keyboard():
+    keyboard = types.ReplyKeyboardMarkup()
+    button = types.KeyboardButton("Share Position", request_location=True)
+    keyboard.add(button)
+    return keyboard
+
+
+@dp.message_handler(content_types=['location'])
+async def handle_location(message: types.Message):
+    lat = message.location.latitude
+    lon = message.location.longitude
+    reply = "latitude:  {}\nlongitude: {}".format(lat, lon)
+    await message.answer(reply, reply_markup=types.ReplyKeyboardRemove())
+
+
+@dp.message_handler(commands=['locate_me'])
+async def cmd_locate_me(message: types.Message):
+    reply = "Click on the the button below to share your location"
+    await message.answer(reply, reply_markup=get_keyboard())
+'''
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
+'''
